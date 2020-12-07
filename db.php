7803beaca03ab822754215deebb7be8834cfed23
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2020-12-07 22:35:46 +0700

    create db.php

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+    $host = 'localhost';
+    $user = 'hasan';
+    $pass = 'kopiko';
+    $db = 'pas_smks';
+
+    $link = mysqli_connect($host, $user, $pass, $db) or die(mysqli_error());
+?>
